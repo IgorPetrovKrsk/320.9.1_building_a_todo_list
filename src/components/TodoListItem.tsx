5f8eb8cae1c3e50d78bb8c31c0b67e283df9652c
IgorPetrovKrsk <igor.petrov.krsk@gmail.com>
2025-05-17 22:08:20 -0600

    edit toogle and cancel buttons done

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -1,5 +1,5 @@
 import type { todoListItemInterface } from "../interfaces/todoListItemInterface";
-import {TodoListDispatchContext} from '../App'
+import { TodoListDispatchContext } from '../App'
 import styles from './todoListItem.module.css'
 import { useContext, useState } from "react";
 
@@ -7,17 +7,31 @@ interface TodoListItemProps {
   todoItem: todoListItemInterface;
 }
 
-export default function TodoListItem( {todoItem}:TodoListItemProps) {
-    const todoListDispatchContext = useContext(TodoListDispatchContext);
-    const [todo,setTodo] = useState(todoItem);
-    
-    return (<>
-        <tr className={styles.todoListItem}>
-          <td> <input onChange={() => todoListDispatchContext?.({type:'SWITCH', payload:todoItem})} type="checkbox" checked={todoItem.completed} /></td>
-          <td>{todoItem.todo}</td>
-          <td><button>Edit</button></td>
-          <td><button onClick={() => todoListDispatchContext?.({type:'DELETE', payload:todoItem})}>Delete</button></td>
-          </tr>
-    </>
-    )
+export default function TodoListItem({ todoItem }: TodoListItemProps) {
+  const todoListDispatchContext = useContext(TodoListDispatchContext);
+  const [todo, setTodo] = useState({ todo: todoItem.todo });
+  const [toggleEdit, setToggleEdit] = useState(false);
+
+  function onEditClick() {
+    setToggleEdit(s => s = !s);
+  }
+  function onCancelClick() {
+    setToggleEdit(s => s = !s);
+    setTodo({ ...todo, todo: todoItem.todo }); //restoring previous value
+  }
+  function onTodoChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setTodo({ ...todo, [event.target.name]: event.target.value });
+  };
+
+
+
+  return (<>
+    <tr className={styles.todoListItem}>
+      <td> <input onChange={() => todoListDispatchContext?.({ type: 'SWITCH', payload: todoItem })} type="checkbox" checked={todoItem.completed} /></td>
+      <td>{toggleEdit ? <input  onChange={onTodoChange} type="text" name='todo' value={todo.todo} /> : todoItem.todo}</td>
+      <td>{toggleEdit ? <button className={styles.btnSave} onClick={onEditClick}>Save</button> : <button onClick={onEditClick}>Edit</button>} </td>
+      <td>{toggleEdit ? <button className={styles.btnCancel} onClick={onCancelClick}>Cancel</button> : <button onClick={() => todoListDispatchContext?.({ type: 'DELETE', payload: todoItem })}>Delete</button>}</td>
+    </tr>
+  </>
+  )
 }
